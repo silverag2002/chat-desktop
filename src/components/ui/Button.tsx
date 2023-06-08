9cfd41174f121b88736f1946ef5fcc1098e08de6
silverag2002 <silverag2002@gmail.com>
2023-06-09 01:56:04 +0530

    Added loader

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,32 +1,50 @@
-import { FC } from "react";
-import cva from "class-variance-authority";
+import { ButtonHTMLAttributes, FC } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
 
-interface ButtonProps {}
+const buttonVariants = cva(
+  [
+    "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disable:opacity-50 disabled:pinter-events-none",
+  ],
+  {
+    variants: {
+      intent: {
+        default: "bg-slate-900 text-white hover:bg-slate-800",
+        ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2",
+        lg: "h-11 px-8",
+      },
+    },
+    defaultVariants: {
+      intent: "default",
+      size: "default",
+    },
+  }
+);
 
-const Button: FC<ButtonProps> = ({}) => {
-  const buttonVariants = cva(
-    [
-      "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disable:opacity-50 disabled:pinter-events-none",
-    ],
-    {
-      variants: {
-        intent: {
-          default: "bg-slate-900 text-white hover:bg-slate-800",
-          ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
-        },
-        size: {
-          default: "h-10 py-2 px-4",
-          sm: "h-9 px-2",
-          lg: "h-11 px-8",
-        },
-      },
-      defaultVariants: {
-        intent: "default",
-        size: "default",
-      },
-    }
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({
+  className,
+  intent,
+  size,
+  children,
+  isLoading,
+  ...props
+}) => {
+  return (
+    <button {...props} className="" disabled={isLoading}>
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}{" "}
+      {children}
+    </button>
   );
-  return <div>Button</div>;
 };
 
 export default Button;
